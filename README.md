4532958bf8baa612d14dd745ca5cdaefdb996ac4
vlod <vladalex420@gmail.com>
2019-03-02 19:17:09 +0200

    identified the current window

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 "# mouseMover" 
+
+this simple program will make it easy to move the cursor across several screens
--- main.cpp
+++ main.cpp
@@ -5,14 +5,16 @@
 #include <windows.h>
 #include <iostream>
 #include <vector>
+#include <algorithm>
 
 #define DISPLAY(x) std::cout << #x << ": " << x
 
-std::vector<HMONITOR> monitors;
+std::vector<std::pair<HMONITOR, MONITORINFO>> monitors;
 
 BOOL CALLBACK registerMonitor(HMONITOR hMonitor, HDC hdc, LPRECT rect, LPARAM data)
 {
-	monitors.push_back(hMonitor);
+	MONITORINFO temp;
+	monitors.emplace_back(hMonitor, temp);
 	return 1;
 }
 
@@ -20,45 +22,71 @@ BOOL CALLBACK registerMonitor(HMONITOR hMonitor, HDC hdc, LPRECT rect, LPARAM da
 
 int main()
 {
-	EnumDisplayMonitors(NULL, NULL, registerMonitor, 0);
+	std::cout << "\n";
 
+	EnumDisplayMonitors(NULL, NULL, registerMonitor, 0);
+	
 	std::cout << "number of visible screens: " << GetSystemMetrics(SM_CMONITORS) << "\n";
 	std::cout << "number of screens: " << monitors.size() << "\n\n";
 
 	int c = 0;
-	for(auto i: monitors)
+	for(auto &i: monitors)
 	{
-		MONITORINFOEX info;
-		info.cbSize = sizeof(info);
-		if (!GetMonitorInfo(i, &info))
+		
+		i.second.cbSize = sizeof(i.second);
+		if (!GetMonitorInfo(i.first, &i.second))
 		{
 			std::cout << "error getting some monitor data: " << GetLastError() << "\n";
 		}else
 		{
-			std::cout << "Monitor: " << c << " " << info.szDevice << "\n";
-			DISPLAY(info.rcMonitor.bottom) << " ";
-			DISPLAY(info.rcMonitor.top) << " ";
-			DISPLAY(info.rcMonitor.left) << " ";
-			DISPLAY(info.rcMonitor.right) << "\n";
+			
+			//std::cout << "Monitor: " << c << " " << info.second.szDevice << "\n";
+			DISPLAY(i.second.rcMonitor.bottom) << " ";
+			DISPLAY(i.second.rcMonitor.top) << " ";
+			DISPLAY(i.second.rcMonitor.left) << " ";
+			DISPLAY(i.second.rcMonitor.right) << "\n";
 
-			DISPLAY(info.rcWork.bottom) << " ";
-			DISPLAY(info.rcWork.top) << " ";
-			DISPLAY(info.rcWork.left) << " ";
-			DISPLAY(info.rcWork.right) << "\n";
+			DISPLAY(i.second.rcWork.bottom) << " ";
+			DISPLAY(i.second.rcWork.top) << " ";
+			DISPLAY(i.second.rcWork.left) << " ";
+			DISPLAY(i.second.rcWork.right) << "\n";
 
-			std::cout << ((info.dwFlags & MONITORINFOF_PRIMARY) ? "Primary Monitor" : "Non primary Monitor");
+			std::cout << ((i.second.dwFlags & MONITORINFOF_PRIMARY) ? "Primary Monitor" : "Non primary Monitor");
 
 			std::cout << "\n\n";
+			
 		}
 
 		c++;
 	}
+	
 
+	std::sort(monitors.begin(), monitors.end(), [](std::pair<HMONITOR, MONITORINFO> i, std::pair<HMONITOR, MONITORINFO> j)
+	{
+		return (i.second.rcMonitor.left < j.second.rcMonitor.left);
+	});
 
 	bool pressed = 0;
 
+	std::cout << monitors[0].second.rcMonitor.right << " ";
+	std::cout << monitors[1].second.rcMonitor.right << " ";
+
 	while (1)
 	{
+		int monitorIndex = -1;
+		POINT mousePosition;
+		GetCursorPos(&mousePosition);
+
+		for(int i=0; i<monitors.size(); i++)
+		{
+			if(mousePosition.x > monitors[i].second.rcMonitor.left)
+			{
+				monitorIndex = i;
+			}
+		}
+
+		SetConsoleCursorPosition(GetStdHandle(STD_OUTPUT_HANDLE), { 0,0 });
+		std::cout << monitorIndex << " ";
 
 		if(pressed == 0 && GetAsyncKeyState(VK_NUMPAD7))
 		{
