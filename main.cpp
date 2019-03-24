82d4e7c51c91bf2d89f87f804cef6824f8fec752
vlod <vladalex420@gmail.com>
2019-03-24 16:21:30 +0200

    switched to a windows application

--- main.cpp
+++ main.cpp
@@ -106,13 +106,107 @@ void increase()
 
 }
 
-int main()
+LRESULT CALLBACK WndProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
 {
+	switch (uMsg)
+	{
+	case WM_DESTROY:
+		PostQuitMessage(0);
+		ExitProcess(0);
+		break;
+	case WM_DEVICECHANGE:
+		loadMonitors();
+	default:
+		return DefWindowProc(hwnd, uMsg, wParam, lParam);
+		break;
+	}
+
+	return 0;
+}
+
+int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nCmdShow){
+	
+	LPCSTR szWindowClass = "window";
+
+	WNDCLASSEX wcex;
+
+	wcex.cbSize = sizeof(WNDCLASSEX);
+	wcex.style = CS_HREDRAW | CS_VREDRAW;
+	wcex.lpfnWndProc = WndProc;
+	wcex.cbClsExtra = 0;
+	wcex.cbWndExtra = 0;
+	wcex.hInstance = hInstance;
+	wcex.hIcon = LoadIcon(hInstance, IDI_APPLICATION);
+	wcex.hCursor = LoadCursor(NULL, IDC_ARROW);
+	wcex.hbrBackground = (HBRUSH)(COLOR_WINDOW + 1);
+	wcex.lpszMenuName = NULL;
+	wcex.lpszClassName = szWindowClass;
+	wcex.hIconSm = LoadIcon(wcex.hInstance, IDI_APPLICATION);
+
+	if (!RegisterClassEx(&wcex))
+	{
+		MessageBox(NULL,
+			"Call to RegisterClassEx failed!",
+			"Windows Desktop Guided Tour",
+			NULL);
+
+		return 1;
+	}
+
+
+	// The parameters to CreateWindow explained:
+	// szWindowClass: the name of the application
+	// szTitle: the text that appears in the title bar
+	// WS_OVERLAPPEDWINDOW: the type of window to create
+	// CW_USEDEFAULT, CW_USEDEFAULT: initial position (x, y)
+	// 500, 100: initial size (width, length)
+	// NULL: the parent of this window
+	// NULL: this application dows not have a menu bar
+	// hInstance: the first parameter from WinMain
+	// NULL: not used in this application
+	HWND hWnd = CreateWindow(
+		szWindowClass,
+		"mouseMover",
+		WS_OVERLAPPEDWINDOW,
+		CW_USEDEFAULT, CW_USEDEFAULT,
+		500, 500,
+		NULL,
+		NULL,
+		hInstance,
+		NULL
+	);
+
+	if (!hWnd)
+	{
+		MessageBox(NULL,
+			"Call to CreateWindow failed!",
+			"Windows Desktop Guided Tour",
+			NULL);
+
+		return 1;
+	}
+
+	ShowWindow(hWnd, nCmdShow);
+	UpdateWindow(hWnd);
+
+	// Main message loop:
+
+
 	std::cout << "\n";
 	
-	//std::cout << "number of visible screens: " << GetSystemMetrics(SM_CMONITORS) << "\n";
-	//std::cout << "number of screens: " << monitors.size() << "\n\n";
-	
+	HCURSOR defaultCursor = GetCursor();
+	struct RAII_
+	{
+		HCURSOR c;
+		void(*function)(HCURSOR c);
+		~RAII_() { function(c); }
+	}instance{defaultCursor, [](HCURSOR c)
+	{
+		SetCursor(c);
+	} };
+
+	auto a = [defaultCursor]() {};
+
 	loadMonitors();
 
 	bool pressed1 = 0;
@@ -121,9 +215,13 @@ int main()
 
 	while (1)
 	{
+		MSG msg;
 
-
+		GetMessage(&msg, NULL, 0, 0);
+		TranslateMessage(&msg);
+		DispatchMessage(&msg);
 		
+
 		//SetConsoleCursorPosition(GetStdHandle(STD_OUTPUT_HANDLE), { 0,0 });
 		//std::cout << monitorIndex << " ";
 
@@ -139,7 +237,7 @@ int main()
 				pressed1 = 0;
 			}
 		}
-
+		
 		if (pressed2 == 0 && GetAsyncKeyState('E'))
 		{
 			pressed2 = 1;
@@ -155,4 +253,5 @@ int main()
 
 	}
 
+	return 0;
 }
